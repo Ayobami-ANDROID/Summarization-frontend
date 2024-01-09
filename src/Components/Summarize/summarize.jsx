5e5e376120e90c0bedbf488d0a19468b9d60621b
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-01-09 10:09:39 +0100

    seterror

--- src/Components/Summarize/summarize.jsx
+++ src/Components/Summarize/summarize.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './Summarize.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./Summarize.css";
+import axios from "axios";
 
 const SummarizerComponent = () => {
-  const [sentences, setSentences] = useState('');
-  const [summary, setSummary] = useState('');
-  const [error, setError] = useState('');
+  const [sentences, setSentences] = useState("");
+  const [summary, setSummary] = useState("");
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event) => {
@@ -15,34 +15,42 @@ const SummarizerComponent = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-  
+
     if (!sentences.trim()) {
-      setError('Input cannot be empty.');
-      setSummary('');
-      setLoading(false)
+      setError("Input cannot be empty.");
+      setSummary("");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+      setLoading(false);
       return;
     }
-  
+
     if (sentences.length <= 40) {
-      setError('Input text must be more than 40 characters.');
-      setSummary('');
+      setError("Input text must be more than 40 characters.");
+      setSummary("");
       return;
     }
-  
+
     try {
-      const response = await axios.post('http://localhost:7000/v1/summarize', { sentences });
-  
+      const response = await axios.post("http://localhost:7000/v1/summarize", {
+        sentences,
+      });
+
       const data = response.data;
       setSummary(data.summary_text);
-      setError('');
+      setError("");
     } catch (error) {
-      setError(error.response?.data?.error || 'An error occurred.');
-      setSummary('');
-    }finally{
-      setLoading(false)
+      setError(error.response?.data?.error || "An error occurred.");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+      setSummary("");
+    } finally {
+      setLoading(false);
     }
   };
-  
+
   return (
     <div className="centering body__wrapper">
       <h1 className="title mb-24">Text Summarization</h1>
@@ -63,7 +71,7 @@ const SummarizerComponent = () => {
             className="centering mb-24"
             type="submit"
           >
-            {loading ? 'Loading...' : 'Summarize'}
+            {loading ? "Loading..." : "Summarize"}
           </button>
           <p id="errorText">{error}</p>
         </form>
